42b84dd1b02e642be8263f0106c8f1f73dacfd42
C1u3 <108814268+C1u3@users.noreply.github.com>
2024-02-10 12:07:42 +0000

    Added GameObject Class

--- main.js
+++ main.js
@@ -9,107 +9,45 @@ import { walls } from "./src/levels/level1.js";
 import { Animations } from "./src/Animations.js";
 import { FrameIndexPattern } from "./src/FrameIndexPattern.js";
 import { WALK_UP, WALK_DOWN, WALK_LEFT, WALK_RIGHT, STAND_UP, STAND_DOWN, STAND_LEFT, STAND_RIGHT } from "./src/objects/Hero/heroAnimations.js";
+import { GameObject } from "./src/GameObject.js";
+import { Hero } from "./src/objects/Hero/Hero.js";
 
+// Grabbing canvas to draw
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
 
+// Establish Root Scene
+const mainScene = new GameObject({
+  position: new Vector2(0, 0),
+});
+
+// Adding sky, ground and hero to scene
 const skySprite = new Sprite({
   resource: resources.images.sky,
   frameSize: new Vector2(320, 180),
 });
+mainScene.addChild(skySprite);
 
 const groundSprite = new Sprite({
   resource: resources.images.ground,
   frameSize: new Vector2(320, 180),
 });
+mainScene.addChild(groundSprite);
 
-const hero = new Sprite({
-  resource: resources.images.hero,
-  frameSize: new Vector2(32, 32),
-  hFrames: 3,
-  vFrames: 8,
-  frame: 1,
-  position: new Vector2(gridCells(6), gridCells(5)),
-  animations: new Animations({
-    standUp: new FrameIndexPattern(STAND_UP),
-    standDown: new FrameIndexPattern(STAND_DOWN),
-    standLeft: new FrameIndexPattern(STAND_LEFT),
-    standRight: new FrameIndexPattern(STAND_RIGHT),
-    walkUp: new FrameIndexPattern(WALK_UP),
-    walkDown: new FrameIndexPattern(WALK_DOWN),
-    walkLeft: new FrameIndexPattern(WALK_LEFT),
-    walkRight: new FrameIndexPattern(WALK_RIGHT),
-  })
-});
+const hero = new Hero(gridCells(6), gridCells(5));
+mainScene.addChild(hero);
 
-const heroDestinationPosition = hero.position.duplicate();
-let heroFacing = DOWN;
+// Adding input class to scene
+mainScene.input = new Input();
 
-const shadow = new Sprite({
-  resource: resources.images.shadow,
-  frameSize: new Vector2(32, 32),
-});
-
-const input = new Input();
-
+// Establish update and draw loops
 const update = (delta) => {
-  const distance = moveTowards(hero, heroDestinationPosition, 1);
-  const hasArrived = distance <= 1;
-
-  if (hasArrived) tryMove()
-
-  hero.step(delta);
+  mainScene.stepEntry(delta, mainScene)
 }
-
-const tryMove = () => {
-  if (!input.direction) {
-    if (heroFacing === UP) hero.animations.play("standUp");
-    if (heroFacing === DOWN) hero.animations.play("standDown");
-    if (heroFacing === LEFT) hero.animations.play("standLeft");
-    if (heroFacing === RIGHT) hero.animations.play("standRight");
-
-    return;
-  }
-
-  let nextX = heroDestinationPosition.x;
-  let nextY = heroDestinationPosition.y;
-  const gridSize = 16;
-
-  if (input.direction === UP) {
-    nextY -= gridSize;
-    hero.animations.play("walkUp");
-  }
-  if (input.direction === DOWN) {
-    nextY += gridSize;
-    hero.animations.play("walkDown");
-  }
-  if (input.direction === LEFT) {
-    nextX -= gridSize;
-    hero.animations.play("walkLeft");
-  }
-  if (input.direction === RIGHT) {
-    nextX += gridSize;
-    hero.animations.play("walkRight");
-  }
-  heroFacing = input.direction ?? heroFacing;
-
-  if (isSpaceFree(walls, nextX, nextY)) {
-    heroDestinationPosition.x = nextX;
-    heroDestinationPosition.y = nextY;
-  }
-}
-
 const draw = () => {
-  skySprite.drawImage(ctx, 0, 0);
-  groundSprite.drawImage(ctx, 0, 0);
-
-  const heroOffset = new Vector2(-8, -21);
-  const heroPosX = hero.position.x + heroOffset.x;
-  const heroPosY = hero.position.y + 1 + heroOffset.y;
-
-  shadow.drawImage(ctx, heroPosX, heroPosY);
-  hero.drawImage(ctx, heroPosX, heroPosY);
+  mainScene.draw(ctx, 0, 0);
 }
 
+// Starting the game
 const gameLoop = new GameLoop(update, draw);
 gameLoop.start();
--- src/GameObject.js
+++ src/GameObject.js
@@ -6,7 +6,36 @@ export class GameObject {
     this.children = [];
   }
 
-  step(delta) {
-    
+  stepEntry(delta, root) {
+    this.children.forEach((child) => child.stepEntry(delta, root));
+
+    this.step(delta, root);
+  }
+
+  step(_delta) {
+    // ...
+  }
+
+  draw(ctx, x, y) {
+    const drawPosX = x + this.position.x;
+    const drawPosY = y + this.position.y;
+
+    this.drawImage(ctx, drawPosX, drawPosY);
+
+    this.children.forEach((child) => child.draw(ctx, drawPosX, drawPosY));
+  }
+
+  drawImage(ctx, drawPosX, drawPosY) {
+    //...
+  }
+
+  addChild(gameObject) {
+    this.children.push(gameObject);
+  }
+
+  removeChild(gameObject) {
+    this.children = this.children.filter(g => {
+      return gameObject !== g;
+    })
   }
 }
